b02945494189ed0b156d148a3fe7cd7001681a54
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-08-03 11:48:17 +0530

    + fix components registry

--- app/components.registry.js
+++ app/components.registry.js
@@ -1,13 +1,12 @@
-import React from "react";
 import CheckUserExist from "./components/CheckUserExist";
 import LoginWithOtp from "./components/LoginWithOtp";
 import LoginWithPassword from "./components/LoginWithPassword";
 import RegisterOrResetPassword from "./components/RegisterOrResetPassword";
 
 export const ComponentRegistry = {
-  checkUserExist: { comp: CheckUserExist },
-  loginWithOtp: { comp: LoginWithOtp },
+  checkUserExist   : { comp: CheckUserExist },
+  loginWithOtp     : { comp: LoginWithOtp },
   loginWithPassword: { comp: LoginWithPassword },
-  register: { comp: RegisterOrResetPassword },
-  resetPassword: { comp: RegisterOrResetPassword },
+  register         : { comp: RegisterOrResetPassword },
+  resetPassword    : { comp: RegisterOrResetPassword },
 };
