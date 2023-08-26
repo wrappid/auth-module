9e9b1c5293c97e17f242b7fe5cdb978103d12156
Pritam Mondal <pritammondal8981@gmail.com>
2023-08-26 14:17:28 +0530

    + middleware added to controller registry

--- service/controllers.registry.js
+++ service/controllers.registry.js
@@ -1,15 +1,45 @@
-const authController = require("./controllers/auth.controller")
+const authController = require("./controllers/auth.controller");
+const { CoreMiddlewaresRegistry } = require("@wrappid/service-core");
+const {
+  checkLoginOrRegister,
+  login,
+  postLoginWithOtp,
+  postLogoutSchema,
+  getIpSchema,
+  refreshTokenSchema,
+  postLoginWithUrl,
+  getClientLoginInfo,
+  sentOtp,
+} = require("./validations/auth.validation");
 
 const controllersRegistry = {
-    checkLoginOrRegister: authController.checkLoginOrRegister,
-    login: authController.login,
-    logout: authController.logout,
-    loginWithOtp: authController.loginWithOtp,
-    getIP: authController.getIP,
-    loginWithUrl: authController.loginWithUrl,
-    clientLoginInformation: authController.clientLoginInformation,
-    refreshToken: authController.refreshToken,
-    sentOtp: authController.sentOtp
+  checkLoginOrRegister: [
+    CoreMiddlewaresRegistry.validation(checkLoginOrRegister),
+    authController.checkLoginOrRegister,
+  ],
+  login: [CoreMiddlewaresRegistry.validation(login), authController.login],
+  logout: [CoreMiddlewaresRegistry.validation(postLogoutSchema), authController.logout],
+  loginWithOtp: [CoreMiddlewaresRegistry.validation(postLoginWithOtp),authController.loginWithOtp],
+  getIP: [
+    CoreMiddlewaresRegistry.validation(getIpSchema),
+    authController.getIP,
+  ],
+  loginWithUrl: [
+    CoreMiddlewaresRegistry.validation(postLoginWithUrl),
+    authController.loginWithUrl,
+  ],
+  clientLoginInformation: [
+    CoreMiddlewaresRegistry.validation(getClientLoginInfo),
+    authController.clientLoginInformation,
+  ],
+  refreshToken: [
+    CoreMiddlewaresRegistry.validation(refreshTokenSchema),
+    authController.refreshToken,
+  ],
+  sentOtp: [
+    CoreMiddlewaresRegistry.validation(sentOtp),
+    authController.sentOtp,
+  ],
 };
 
 exports.controllersRegistry = controllersRegistry;
