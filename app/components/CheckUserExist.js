a12b679694967f4d9ca4c3f4fd6fa663c4cbd1a0
sumanta-m <sumanta.m@vixplor.com>
2023-08-28 22:10:09 +0530

    bug fix

--- app/components/CheckUserExist.js
+++ app/components/CheckUserExist.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   CoreForm,
   CoreClasses,
-  CoreDomNavigate
+  CoreDomNavigate,
+  CoreRouteRegistryContext
 } from "@wrappid/core";
 
 import { AuthContainer } from "./AuthContainer";
