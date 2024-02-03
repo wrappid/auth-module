1027f702e7e5260b7442ec2f2e3dadcd598c5507
Nilotpal Saha <nilotpal.saha00@gmail.com>
2024-02-04 01:17:30 +0530

    fix(core): :art: refactor: linting issue fix for app
    linting issue fixed
    
    Ref #14

--- app/constants/app.constants.js
+++ app/constants/app.constants.js
@@ -1,9 +1,9 @@
 export const ModuleRoute = {
-  LOGIN_OTP_ROUTE: "enterOtp",
-  LOGIN_ROUTE: "checkUserExist",
-  LOGOUT: "logout",
-  PASSWORD_ROUTE: "enterPassword",
-  REGISTER_ROUTE: "register",
+  LOGIN_DASHBOARD     : "dashboard",
+  LOGIN_OTP_ROUTE     : "enterOtp",
+  LOGIN_ROUTE         : "checkUserExist",
+  LOGOUT              : "logout",
+  PASSWORD_ROUTE      : "enterPassword",
+  REGISTER_ROUTE      : "register",
   RESET_PASSWORD_ROUTE: "resetPassword",
-  LOGIN_DASHBOARD: "dashboard",
 };
